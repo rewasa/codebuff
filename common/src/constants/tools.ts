8bb59fa35e91ae1f01aa562770fe39fea23812d7
James Grugett <jahooma@gmail.com>
2025-06-30 13:46:50 -0700

    Add missing parms to code_search tool schema

--- common/src/constants/tools.ts
+++ common/src/constants/tools.ts
@@ -10,7 +10,7 @@ export const toolSchema = {
   find_files: ['description'],
 
   // Search and terminal
-  code_search: ['pattern'],
+  code_search: ['pattern', 'flags', 'cwd'],
   run_terminal_command: ['command', 'process_type', 'cwd', 'timeout_seconds'],
 
   // Planning tools
