0028bf936aba156aacb5da6a89b367d55d3a5aea
James Grugett <jahooma@gmail.com>
2025-07-02 11:51:02 -0700

    [BuffBench] New relace

--- backend/src/llm-apis/relace-api.ts
+++ backend/src/llm-apis/relace-api.ts
@@ -76,8 +76,8 @@ export async function promptRelaceAI(
       clientSessionId,
       fingerprintId,
       userInputId,
-      model: 'relace-fast-apply',
-      // model: 'relace-apply-2.5-lite',
+      // model: 'relace-fast-apply',
+      model: 'relace-apply-2.5-lite',
       request: [
         {
           role: 'user',
