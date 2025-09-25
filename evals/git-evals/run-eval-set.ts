76eced457b8ff424ffe75b5ada2daaabdef196a8
James Grugett <jahooma@gmail.com>
2025-09-24 20:50:30 -0700

    [buffbench] redo grok-4-fast eval

--- evals/git-evals/run-eval-set.ts
+++ evals/git-evals/run-eval-set.ts
@@ -72,7 +72,7 @@ class RunEvalSetCommand extends Command {
     }),
     agent: Flags.string({
       description: 'Codebuff agent id to use',
-      default: 'base-lite-grok-4-fast',
+      default: 'base-lite-grok-4-fast', // hi
     }),
     help: Flags.help({ char: 'h' }),
   }
