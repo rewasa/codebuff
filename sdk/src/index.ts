8a2bb28c4f5b6713dadcd992c485138b7148db94
Daniel Hsu <dannyighsu@gmail.com>
2025-10-10 14:17:29 -0700

    Bug: Expose types in CodebuffClientOptions (#340)
    Co-authored-by: James Grugett <jahooma@gmail.com>

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,15 +1,18 @@
-export type * from './run'
 export type * from '../../common/src/types/json'
 export type * from '../../common/src/types/messages/codebuff-message'
 export type * from '../../common/src/types/messages/data-content'
+export type * from '../../common/src/types/print-mode'
+export type * from './run'
 // Agent type exports
 export type { AgentDefinition } from '../../common/src/templates/initial-agents-dir/types/agent-definition'
 // Re-export code analysis functionality
 export * from '../../packages/code-map/src/index'
 
+export type { ClientToolCall, ClientToolName, CodebuffToolOutput } from '../../common/src/tools/list'
 export * from './client'
 export * from './custom-tool'
-export * from './run-state'
-export * from './websocket-client'
 export * from './native/ripgrep'
-export { ToolHelpers } from './tools'
+export * from './run-state'
+export { ToolHelpers, ToolName } from './tools'
+export * from './websocket-client'
+
